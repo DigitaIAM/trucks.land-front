2683712ec8dfe01c769f2388e3b6ed6c49cc606d
Natalya Shabanova <shabanovanatali@gmail.com>
2026-07-24 13:06:54 +0500

    refactoring

--- src/pages/[oid]/paymentsAndExpenses/settlements_to_employee.vue
+++ src/pages/[oid]/paymentsAndExpenses/settlements_to_employee.vue
@@ -6,6 +6,8 @@ meta:
 
 <script lang="ts">
 import { defineBasicLoader } from 'unplugin-vue-router/data-loaders/basic'
+import type { KV } from '@/utils/kv.ts'
+import moment from 'moment-timezone'
 
 const organizationsStore = useOrganizationsStore()
 const authStore = useAuthStore()
@@ -16,8 +18,7 @@ export const useOrgData = defineBasicLoader(
   async (route) => {
     const org = await organizationsStore.resolve3(route.params.oid)
     authStore.org = org
-    await settlementsEmployeeStore.loading(org.id)
-    // console.table(org)
+    await settlementsEmployeeStore.setContext([{ key: 'organization', val: org.id } as KV])
     return org
   },
   { key: 'org' },
@@ -32,6 +33,8 @@ const employeeSettlementsTypeStore = useEmployeeSettlementsTypeStore()
 const selectedDocument = ref<SettlementEmployee | null>(null)
 const selectedTypes = ref<Array<EmployeeSettlementsType> | null>(null)
 const allowEmployeeSelection = ref<boolean>(false)
+
+const filters = ref<Array<KV>>([])
 
 defineOptions({
   __loaders: [useOrgData],
@@ -141,6 +144,85 @@ function createSettlement() {
     notes: '',
   } as unknown as SettlementEmployee
 }
+
+function setFilter(key: string, val: any) {
+  const index = filters.value.findIndex((v) => v.key === key)
+  if (index < 0) {
+    filters.value.push({ key: key, val: val } as KV)
+  } else {
+    filters.value[index] = { key: key, val: val } as KV
+  }
+
+  if (key === 'month') {
+    const now = moment().tz('America/New_York')
+    const year = val.id > now.month() ? now.year() - 1 : now.year()
+
+    const nkey = 'year'
+    const nval = { id: year, name: `${year}` }
+
+    const nindex = filters.value.findIndex((v) => v.key === nkey)
+    if (nindex < 0) {
+      filters.value.push({ key: nkey, val: nval } as KV)
+    } else {
+      filters.value[nindex] = { key: nkey, val: nval } as KV
+    }
+  }
+
+  _applyFilters()
+}
+
+function delFilter(key: string) {
+  const index = filters.value.findIndex((v) => v.key === key)
+  if (index >= 0) {
+    filters.value.splice(index, 1)
+  }
+
+  _applyFilters()
+}
+
+function _applyFilters() {
+  const resolved: Array<KV> = []
+
+  const monthFilter = filters.value.find((v) => v.key === 'month')
+  const yearFilter = filters.value.find((v) => v.key === 'year')
+
+  if (monthFilter && yearFilter) {
+    const month = typeof monthFilter.val === 'object' ? monthFilter.val.id : parseInt(monthFilter.val)
+    const year = typeof yearFilter.val === 'object' ? yearFilter.val.id : parseInt(yearFilter.val)
+
+    const start = moment({ year, month: month - 1, day: 1 }).startOf('month').toISOString()
+    const end = moment({ year, month: month - 1, day: 1 }).endOf('month').add(1, 'day').toISOString()
+
+    resolved.push({ key: 'created_at_range', val: { start, end } } as KV)
+  } else if (monthFilter) {
+    const month = typeof monthFilter.val === 'object' ? monthFilter.val.id : parseInt(monthFilter.val)
+    const now = moment()
+    const year = month > now.month() + 1 ? now.year() - 1 : now.year()
+
+    const start = moment({ year, month: month - 1, day: 1 }).startOf('month').toISOString()
+    const end = moment({ year, month: month - 1, day: 1 }).endOf('month').add(1, 'day').toISOString()
+
+    resolved.push({ key: 'created_at_range', val: { start, end } } as KV)
+  } else if (yearFilter) {
+    const year = typeof yearFilter.val === 'object' ? yearFilter.val.id : parseInt(yearFilter.val)
+
+    const start = moment({ year }).startOf('year').toISOString()
+    const end = moment({ year }).endOf('year').add(1, 'day').toISOString()
+
+    resolved.push({ key: 'created_at_range', val: { start, end } } as KV)
+  }
+
+  const employeeFilter = filters.value.find((v) => v.key === 'employee')
+  if (employeeFilter) {
+    resolved.push(employeeFilter)
+  }
+
+  settlementsEmployeeStore.setFilters(resolved)
+}
+
+function capitalizeFirstLetter(val: any) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1)
+}
 </script>
 
 <template>
@@ -155,6 +237,27 @@ function createSettlement() {
     <Button class="btn-soft font-light tracking-wider mr-4" @click="createSettlement"
       >Create</Button
     >
+  </div>
+  <div class="flex flex-row gap-6 px-4 mb-2 mt-3">
+    <SearchForPaymentsDispatcher @selected="setFilter"></SearchForPaymentsDispatcher>
+  </div>
+  <div class="flex flex-row gap-6 px-4 mt-3">
+    <Badge lg ghost v-for="filter in filters" :key="filter.key" @click="delFilter(filter.key)">
+      <div class="font-thin tracking-wider text-sm text-gray-700 uppercase dark:text-gray-400">
+        {{ capitalizeFirstLetter(filter.key) }}:
+      </div>
+      <div>{{ filter.key === 'employee' ? filter.val.real_name : filter.val.name }}</div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="0.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+      </svg>
+    </Badge>
   </div>
   <table class="w-full mt-6 text-left table-auto min-w-max">
     <thead>
--- src/stores/employee_settlements.ts
+++ src/stores/employee_settlements.ts
@@ -1,4 +1,5 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import type { KV } from '@/utils/kv.ts'
 
 export interface SettlementEmployee extends SettlementEmployeeCreate {
   id: number
@@ -26,32 +27,62 @@ export interface SettlementEmployeeUpdate {
 export const useSettlementsEmployeeStore = defineStore('employee_settlements', () => {
   const listing = ref<Array<SettlementEmployee>>([])
 
-  async function loading(orgId: number | null) {
-    console.log('orgId', orgId)
-    if (orgId) {
-      const response = await supabase
-        .from('employee_settlements')
-        .select()
-        .eq('organization', orgId)
-        .order('created_at', { ascending: false })
-        .limit(20)
+  const contextFilters = ref<Array<KV>>([])
+  const searchFilters = ref<Array<KV>>([])
 
-      console.log('employee_settlements', response)
+  const timestamp = ref(Date.now())
 
+  async function setContext(filters: Array<KV>) {
+    contextFilters.value = filters
+    await _setFilters()
+  }
+
+  async function setFilters(filters: Array<KV>) {
+    searchFilters.value = filters
+    await _setFilters()
+  }
+
+  async function _setFilters() {
+    const localTime = Date.now()
+
+    if (timestamp.value > localTime) {
+      return
+    }
+    timestamp.value = localTime
+
+    let query = supabase.from('employee_settlements').select()
+
+    const allFilters = contextFilters.value.concat(searchFilters.value)
+
+    allFilters.forEach((f) => {
+      const x = f.val
+
+      if (f.key === 'created_at_range') {
+        query = query.gte('created_at', x.start).lt('created_at', x.end)
+        return
+      }
+
+      if (typeof x === 'object' && !Array.isArray(x) && x !== null) {
+        query = query.eq(f.key, x.id)
+      } else if (Array.isArray(x)) {
+        query = query.in(f.key, x)
+      } else {
+        query = query.eq(f.key, x)
+      }
+    })
+
+    const response = await query.order('created_at', { ascending: false }).limit(50)
+
+    if (timestamp.value == localTime) {
       if (response.status == 200) {
         const list: Array<SettlementEmployee> = []
-
         response.data?.forEach((json) => {
-          const settlement = json as SettlementEmployee
-          list.push(settlement)
+          list.push(json as SettlementEmployee)
         })
-
         listing.value = list
       } else {
-        throw 'unexpended response status: ' + response.status
+        throw 'unexpected response status: ' + response.status
       }
-    } else {
-      listing.value = [] as Array<SettlementEmployee>
     }
   }
 
@@ -108,7 +139,7 @@ export const useSettlementsEmployeeStore = defineStore('employee_settlements', (
     return null
   }
 
-  return { listing, loading, create, update }
+  return { listing, setContext, setFilters, create, update }
 })
 
 if (import.meta.hot) {
